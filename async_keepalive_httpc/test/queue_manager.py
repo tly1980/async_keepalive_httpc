30c2addb36c0477f30edb560eaa39eede1fe4cb6
Tom Tang <tly1980@gmail.com>
2013-10-29 21:26:06 +1100

    more ut

--- async_keepalive_httpc/test/queue_manager.py
+++ async_keepalive_httpc/test/queue_manager.py
@@ -43,18 +43,21 @@ class UrlInfoTest(unittest.TestCase):
         self.assertEqual(urlinfo.port, 80)
         self.assertEqual(urlinfo.is_ssl, False)
         self.assertEqual(urlinfo.uri_with_query, '/abc?a=23&b=456')
+        self.assertEqual(urlinfo.connection_key, 'http://localhost:80')
 
     def test_https(self):
         urlinfo = UrlInfo('https://localhost/abc')
         self.assertEqual(urlinfo.port, 443)
         self.assertEqual(urlinfo.is_ssl, True)
         self.assertEqual(urlinfo.is_ssl, True)
+        self.assertEqual(urlinfo.connection_key, 'https://localhost:443')
 
-    def test_https(self):
+    def test_https_port(self):
         urlinfo = UrlInfo('https://localhost:9443/abc')
         self.assertEqual(urlinfo.port, 9443)
         self.assertEqual(urlinfo.is_ssl, True)
         self.assertEqual(urlinfo.is_ssl, True)
+        self.assertEqual(urlinfo.connection_key, 'https://localhost:9443')
 
 
 class QueueManagerTestCase(AsyncTestCase):
